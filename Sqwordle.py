48ecc3ea9b7f55873234bf0e470ed9e342a94371
David Caudle <dbcaudle@gmail.com>
2022-02-11 14:04:15 -0500

    Initial Commit

---
+++ Sqwordle.py
@@ -0,0 +1,119 @@
+# sqwordle.py
+import random
+
+from discord.ext import commands
+from dotenv import dotenv_values
+from numpy import average
+
+from SqwordleFunctions import *
+
+temp = dotenv_values(".env") 
+TOKEN = temp['DISCORD_TOKEN']
+
+bot=commands.Bot(command_prefix='!')
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='wordle-gun',  help='Displays team stats for the specified Wordle')
+async def wordle_stats(ctx, *, game_number = ''):
+    guild_id = ctx.message.channel.guild.id
+
+    if len(game_number) == 0:
+        await ctx.send('Try again with game number. (!wordle-gun 230)')
+        return
+
+    total_attempts = 0
+    total_players = 0
+    low_score = 99
+    daily_winners = []
+
+    messages = await ctx.channel.history(limit=400).flatten()
+    for msg in messages:
+        if msg.author.id == 938836891741069382: #self
+            continue
+        
+        checkMsg = 'Wordle ' + game_number
+        if checkMsg in msg.content:
+
+            #for user_id in total_players:
+            #    if msg.author.id == user_id:
+            #        break
+            MsgHeader = msg.content[:14]
+            lhs, rhs = MsgHeader.split('/')
+            tries_taken = lhs[-1]
+            if tries_taken == 'X':
+                tries_taken = 7
+            else:
+                tries_taken = int(tries_taken)
+                if tries_taken < low_score:
+                    low_score = tries_taken
+                    daily_winners.clear()
+                    daily_winners.append(msg.author.id)
+                elif tries_taken == low_score:
+                    daily_winners.append(msg.author.id)
+            
+            filename = 'wordle_stats_' + str(guild_id) + '.csv'
+            RecordStats(filename, msg.author.id, game_number, tries_taken)
+
+            total_attempts = total_attempts + tries_taken
+            total_players = total_players + 1
+    
+    if total_players == 0:
+        await ctx.send('No players for Wordle ' + str(game_number))
+    else:
+        
+        winner_list = ''
+        for winners in daily_winners:
+            if len(winner_list) == 0:
+                winner_list = '<@' + str(winners) + '>'
+            else:
+                winner_list = winner_list + ', <@' + str(winners) + '>'
+
+        average_attempts = total_attempts / total_players
+        average_attempts = format(average_attempts, '.2f')
+
+        sendString = ('Wordle Game ' + str(game_number) + '\n' +
+                      'Total Players: ' + str(total_players) + '\n' +
+                      'Total Guesses: ' + str(total_attempts) + '\n' +
+                      'Average Score: ' + str(average_attempts) + '\n\n' +
+                      'Congratulations to ' + winner_list + ' with a score of ' + 
+                      str(low_score))
+        
+        await ctx.send(sendString)
+
+@bot.command(name='wordle-dex',  help='Displays wordledex entry for the player')
+async def wordle_stats(ctx, *, user = ''):
+    guild_id = ctx.message.channel.guild.id
+
+    # Get user id
+    if len(user) == 0:
+        player_id = ctx.message.author.id
+    elif not user.startswith('<@!'):
+        await ctx.send('Invalid user handle!')
+        return
+    else:
+        player_id = ''
+        # Iterate over characters in user
+        for ch in user:
+            if ch.isdigit():
+                player_id += ch
+
+    filename = 'wordle_stats_' + str(guild_id) + '.csv'
+    sendString = ReadStats(filename, int(player_id))
+    await ctx.send(sendString)
+
+
+bot.run(TOKEN)
---
+++ SqwordleFunctions.py
@@ -0,0 +1,94 @@
+# Additional functions for Sqeordle
+import csv
+import random
+def RecordStats(filename, player, game_number, attempts):
+    with open(filename, mode='a') as wordle_stats:
+        wordledex = csv.writer(wordle_stats, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        wordledex.writerow([player, game_number, attempts])
+
+def ReadStats(filename, player):
+
+    tot_score = 0
+    tot_games = 0
+    worst_game = 0
+    best_game = 9
+    lost_games = 0
+
+    with open(filename, mode='r') as wordle_stats:
+        wordledex = csv.reader(wordle_stats, delimiter=',')
+
+        for row in wordledex:
+            if (row == []) or (int(row[0]) != player):
+                continue
+            else:
+                if int(row[2]) == 7:
+                    lost_games = lost_games + 1
+                else:
+                    tot_score = tot_score + int(row[2])
+                    if int(row[2]) > worst_game:
+                        worst_game = int(row[2])
+                    if int(row[2]) < best_game:
+                        best_game = int(row[2])
+                
+                tot_games = tot_games + 1
+    
+    if tot_games == 0:
+        sendString = 'No stats for <@' + str(player) + '>'
+    else:
+        avg = format(tot_score / tot_games, '.2f')
+        dex_entry = Wordledex_Entry(player)
+
+        sendString = ('Wordle Stats for ' + '<@' + str(player) + '>:' + '\n' +
+                      'Games Played: ' + str(tot_games) + '\n' +
+                      'Average Score: ' + str(avg) + '\n' +
+                      'Best Game: ' + str(best_game) + '\n' +
+                      'Worst Game: ' + str(worst_game) + '\n\n' +
+                      dex_entry)
+    return sendString
+
+def Wordledex_Entry(playerID):
+    player = '<@' + str(playerID) + '>'
+
+    entry = random.randint(1,20)
+    if entry == 1:
+        dex_entry = player + '\'s body temperature is approximately 18,000 degrees F.'
+    elif entry == 2:
+        dex_entry = player + ' feeds on soil. After it has eaten a large mountain, it will fall asleep so it can grow.'
+    elif entry == 3:
+        dex_entry = player + '\'s droppings are hot, so people used to put them in their clothes to keep themselves warm.'
+    elif entry == 4:
+        dex_entry = 'It happened one morning - A boy with extrasensory powers awoke in bed transformed into ' + player + '.'
+    elif entry == 5:
+        dex_entry = 'The spirits ' + player + ' absorbs fuel its baleful fire. It hangs around hospitals waiting for people to pass on.'
+    elif entry == 6:
+        dex_entry = player + ' has the habit of hugging its companions. Many friends have left this world after their spines were squashed by its hug.'
+    elif entry == 7:
+        dex_entry = player + '\'s brain is very small. It is so dense, while on a run it forgets why it started running in the first place. It apparently remembers sometimes if it demolishes something.'
+    elif entry == 8:
+        dex_entry = player + ' is virtually worthless in terms of both power and speed. It is the most weak and pathetic player in the world.'
+    elif entry == 9:
+        dex_entry = 'If a child it has made friends with is bullied, ' + player + ' will find the bully\'s house and burn it to the ground.'
+    elif entry == 10:
+        dex_entry = player + ' is born asleep, and dies asleep. All its movements are apparently no more than the result of it tossing and turning in its dreams.'
+    elif entry == 11:
+        dex_entry = player + ' has a dream of one day soaring in the sky. In doomed efforts to fly, this player hurls itself off cliffs. As a result of its dives, its head has grown tough and as hard as tempered steel.'
+    elif entry == 12:
+        dex_entry = player + ' spins string not only from its rear but also from its mouth. It is hard to tell which end is which.'
+    elif entry == 13:
+        dex_entry = player + ' threatens any attackers by fiercely jingling their keys at them.'
+    elif entry == 14:
+        dex_entry = 'While guarding its weak points with its pincers, ' + player + ' looks for an opening and unleashes punches. When it loses, it foams at the mouth and faints.'
+    elif entry == 15:
+        dex_entry = player + '\'s tackle is forceful enough to flip a 50-ton tank. It shields its allies from danger with its own body.'
+    elif entry == 16:
+        dex_entry = player + ' freezes hikers who have come to climb snowy mountains and carries them back to its home. It only goes after men it thinks are handsome.'
+    elif entry == 17:
+        dex_entry = 'To protect its friends, ' + player + ' will expend all its psychic power to create a small black hole.'
+    elif entry == 18:
+        dex_entry = player + ' can clear a 30-story building in a single leap.'
+    elif entry == 19:
+        dex_entry = 'For some reason, ' + player + ' likes to land on people\'s heads softly and act like it\'s a hat.'
+    elif entry == 20:
+        dex_entry = 'If ' + player + ' bonds with a person, it will gently envelop the friend with its soft wings, then hum.'
+
+    return dex_entry
